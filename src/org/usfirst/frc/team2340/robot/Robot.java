aef5c81c4805c8413790b105f3c73a6480e8389a
Philip Moise <Philip.Moise@Xerox.com>
2018-02-19 14:55:06 -0500

    Last bit of cleanup

--- src/org/usfirst/frc/team2340/robot/Robot.java
+++ src/org/usfirst/frc/team2340/robot/Robot.java
@@ -10,9 +10,7 @@ import org.usfirst.frc.team2340.robot.framework.DebugLogger;
 import org.usfirst.frc.team2340.robot.commands.AutoRotation;
 import org.usfirst.frc.team2340.robot.subsystems.*;
 
-import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -40,39 +38,33 @@ public class Robot extends IterativeRobot {
 	Camera camera = null;
 	SendableChooser<AutoMode> autoMode = new SendableChooser<AutoMode>();
 
-
 	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
 	 */
 	public void robotInit() {
-		System.out.println("robotinit");
 		RobotUtils.lengthOfRobot(39);
 		RobotUtils.setWheelDiameter(4);
 
-//		myLogger.open("/", "DebugLogger", ".csv");
+		// myLogger.open("/", "DebugLogger", ".csv");
 
 		oi.gyro = new ADXRS450_Gyro();
 		camera = new Camera();
 
-		autoMode.addDefault("DriveForward", AutoMode.DriveForward);
+		autoMode.addDefault("DriveForward", AutoMode.DRIVE_FORWARD);
 		autoMode.addObject("Disabled", AutoMode.DISABLED);
-		autoMode.addObject("OneSwitch", AutoMode.OneSwitch);
-		autoMode.addObject("OneScale", AutoMode.OneScale);
-		autoMode.addObject("TwoSwitch", AutoMode.TwoSwitch);
-		autoMode.addObject("ThreeSwitch", AutoMode.ThreeSwitch);
-		autoMode.addObject("ThreeScale", AutoMode.ThreeScale);
+		autoMode.addObject("OneSwitch", AutoMode.ONE_SWITCH);
+		autoMode.addObject("OneScale", AutoMode.ONE_SCALE);
+		autoMode.addObject("TwoSwitch", AutoMode.TWO_SWITCH);
+		autoMode.addObject("ThreeSwitch", AutoMode.THREE_SWITCH);
+		autoMode.addObject("ThreeScale", AutoMode.THREE_SCALE);
 		SmartDashboard.putData("Autonomous Modes", autoMode);
 
-		camera = new Camera(); 
-		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-		System.out.println("Camera name: " + camera.getName());
-		camera.setResolution((int)Robot.oi.IMG_WIDTH, (int)Robot.oi.IMG_HEIGHT);
-
+		camera = new Camera();
 	}
 
-	public void disabledInit(){
-		if(camera != null){
+	public void disabledInit() {
+		if (camera != null) {
 			camera.cancel();
 		}
 	}
@@ -85,153 +77,155 @@ public class Robot extends IterativeRobot {
 		oi.gyro.reset();
 		AutoMode am = (AutoMode) autoMode.getSelected();
 		autonomousCommand = new CommandGroup();
-        //TODO: Disable should be the ruler of all
-		if (DriverStation.getInstance().getGameSpecificMessage().isEmpty()) {
+		String gameData = DriverStation.getInstance().getGameSpecificMessage();
+		myLogger.log("Autonomous", "GameData", gameData);
+		myLogger.log("Autonomous", "Mode", am.toString());
+		if (am == AutoMode.DISABLED) {
+			// Do Nothing if disabled
+		}
+		else if (DriverStation.getInstance().getGameSpecificMessage().isEmpty()) {
 			autonomousCommand.addSequential(new AutoDrive(135));
 		}
-		else {
-		if(am == AutoMode.DriveForward){
-			//Drive forward is relative to the back of the robot
+		else if (am == AutoMode.DRIVE_FORWARD) {
+			// Drive forward is relative to the back of the robot
 			autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(135)));
 		}
-		else if (am == AutoMode.OneSwitch) {
-			String gameData;
-			gameData = DriverStation.getInstance().getGameSpecificMessage();
-			if(gameData.charAt(0) == 'R')
-			{
+		else if (am == AutoMode.ONE_SWITCH) {
+			if (gameData.charAt(0) == 'R') {
 				DriveCorrection corr = new DriveCorrection();
-				//This should bring the center of the robot to the center of the switch
+				// This should bring the center of the robot to the center of the switch
 				// distance from wall to bottom of switch + switch width / 2 - Length of robot
 				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(168), corr, -1));
-				//Rotates to the left 90
+				// Rotates to the left 90
 				autonomousCommand.addSequential(new AutoRotation(-90));
-				//TODO: 19 is a made up value...
+				// TODO: 19 is a made up value...
 				autonomousCommand.addParallel(new AutoElevator(19));
-				//distance from wall to front of switch (d) - Length of robot (l) - back of robot to wall (w) + correction (c)
-				//so, desired distance = d - l - w + c; or, d - l - (w - c)
-				//distance from wall is known ahead of time (how wide is the diagonal piece).
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(55.56 + (.5 * RobotUtils.getLengthOfRobot()) + corr.getCorrection().x)));
+				// distance from wall to front of switch (d) - Length of robot (l) - back of
+				// robot to wall (w) + correction (c)
+				// so, desired distance = d - l - w + c; or, d - l - (w - c)
+				// distance from wall is known ahead of time (how wide is the diagonal piece).
+				autonomousCommand.addSequential(new AutoDrive(RobotUtils
+						.distanceMinusRobot(55.56 + (.5 * RobotUtils.getLengthOfRobot()) + corr.getCorrection().x)));
 				autonomousCommand.addSequential(new AutoArm());
 			}
 			else {
 				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(228.735)));
 				autonomousCommand.addSequential(new AutoRotation(-90));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(264 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(264 + (.5 * RobotUtils.getLengthOfRobot()))));
 				autonomousCommand.addSequential(new AutoRotation(-90));
 				autonomousCommand.addParallel(new AutoElevator(19));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(88.735 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(88.735 + (.5 * RobotUtils.getLengthOfRobot()))));
 				autonomousCommand.addSequential(new AutoRotation(-90));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(55.56 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(55.56 + (.5 * RobotUtils.getLengthOfRobot()))));
 				autonomousCommand.addSequential(new AutoArm());
 			}
 		}
-		else if(am == AutoMode.OneScale){
-			String gameData;
-			gameData = DriverStation.getInstance().getGameSpecificMessage();
-			if(gameData.charAt(1) == 'R')
-			{
+		else if (am == AutoMode.ONE_SCALE) {
+			if (gameData.charAt(1) == 'R') {
 				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(340.5)));
 				autonomousCommand.addSequential(new AutoRotation(-90));
 				autonomousCommand.addParallel(new AutoElevator(51));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(42.06 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(42.06 + (.5 * RobotUtils.getLengthOfRobot()))));
 				autonomousCommand.addSequential(new AutoArm());
 			}
 			else {
 				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(228.735)));
 				autonomousCommand.addSequential(new AutoRotation(-90));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(264 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(264 + (.5 * RobotUtils.getLengthOfRobot()))));
 				autonomousCommand.addSequential(new AutoRotation(90));
 				autonomousCommand.addParallel(new AutoElevator(51));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(106.265 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(106.265 + (.5 * RobotUtils.getLengthOfRobot()))));
 				autonomousCommand.addSequential(new AutoRotation(90));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(42.06 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(42.06 + (.5 * RobotUtils.getLengthOfRobot()))));
 				autonomousCommand.addSequential(new AutoArm());
 			}
 		}
-		else if(am == AutoMode.TwoSwitch){
-			String gameData;
-			gameData = DriverStation.getInstance().getGameSpecificMessage();
-			if(gameData.charAt(0) == 'R'){
+		else if (am == AutoMode.TWO_SWITCH) {
+			if (gameData.charAt(0) == 'R') {
 				autonomousCommand.addParallel(new AutoElevator(19));
 				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(140)));
-				autonomousCommand.addSequential(new AutoArm ( ));
+				autonomousCommand.addSequential(new AutoArm());
 			}
 			else {
 				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(130)));
 			}
 		}
-		else if(am == AutoMode.ThreeSwitch){
-			String gameData;
-			gameData = DriverStation.getInstance().getGameSpecificMessage();
-			if(gameData.charAt(0) == 'R')
-			{
+		else if (am == AutoMode.THREE_SWITCH) {
+			if (gameData.charAt(0) == 'R') {
 				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(228.735)));
-				autonomousCommand.addSequential(new AutoRotation (90));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(264+(.5*RobotUtils.getLengthOfRobot()))));
-				autonomousCommand.addSequential(new AutoRotation (90));
+				autonomousCommand.addSequential(new AutoRotation(90));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(264 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(new AutoRotation(90));
 				autonomousCommand.addParallel(new AutoElevator(19));
-				autonomousCommand.addSequential(new AutoDrive (88.735+(.5*RobotUtils.getLengthOfRobot())));
-				autonomousCommand.addSequential(new AutoRotation (90));
-				autonomousCommand.addSequential(new AutoDrive (55.56+(.5*RobotUtils.getLengthOfRobot())));
-				autonomousCommand.addSequential(new AutoArm ( ));
+				autonomousCommand.addSequential(new AutoDrive(88.735 + (.5 * RobotUtils.getLengthOfRobot())));
+				autonomousCommand.addSequential(new AutoRotation(90));
+				autonomousCommand.addSequential(new AutoDrive(55.56 + (.5 * RobotUtils.getLengthOfRobot())));
+				autonomousCommand.addSequential(new AutoArm());
 			}
 			else {
 				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(168)));
-				autonomousCommand.addSequential(new AutoRotation (90));
+				autonomousCommand.addSequential(new AutoRotation(90));
 				autonomousCommand.addParallel(new AutoElevator(19));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(55.56+(.5*RobotUtils.getLengthOfRobot()))));
-				autonomousCommand.addSequential(new AutoArm ( ));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(55.56 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(new AutoArm());
 			}
 		}
-		else if(am == AutoMode.ThreeScale){
-			String gameData;
-			gameData = DriverStation.getInstance().getGameSpecificMessage();
-			if(gameData.charAt(1) == 'R')
-			{
+		else if (am == AutoMode.THREE_SCALE) {
+			if (gameData.charAt(1) == 'R') {
 				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(228.735)));
-				autonomousCommand.addSequential(new AutoRotation (90));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(264+(.5*RobotUtils.getLengthOfRobot()))));
-				autonomousCommand.addSequential(new AutoRotation (-90));
+				autonomousCommand.addSequential(new AutoRotation(90));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(264 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(new AutoRotation(-90));
 				autonomousCommand.addParallel(new AutoElevator(51));
-				autonomousCommand.addSequential(new AutoDrive (106.265+(.5*RobotUtils.getLengthOfRobot())));
-				autonomousCommand.addSequential(new AutoRotation (-90));
-				autonomousCommand.addSequential(new AutoDrive (42.06+(.5*RobotUtils.getLengthOfRobot())));
-				autonomousCommand.addSequential(new AutoArm ( ));
+				autonomousCommand.addSequential(new AutoDrive(106.265 + (.5 * RobotUtils.getLengthOfRobot())));
+				autonomousCommand.addSequential(new AutoRotation(-90));
+				autonomousCommand.addSequential(new AutoDrive(42.06 + (.5 * RobotUtils.getLengthOfRobot())));
+				autonomousCommand.addSequential(new AutoArm());
 
 			}
 			else {
 				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(340.5)));
-				autonomousCommand.addSequential(new AutoRotation (90));
+				autonomousCommand.addSequential(new AutoRotation(90));
 				autonomousCommand.addParallel(new AutoElevator(51));
-				autonomousCommand.addSequential(new AutoDrive(RobotUtils.distanceMinusRobot(41.88+(.5*RobotUtils.getLengthOfRobot()))));
-				autonomousCommand.addSequential(new AutoArm ( ));
+				autonomousCommand.addSequential(
+						new AutoDrive(RobotUtils.distanceMinusRobot(41.88 + (.5 * RobotUtils.getLengthOfRobot()))));
+				autonomousCommand.addSequential(new AutoArm());
 			}
 		}
-		else if (am == AutoMode.DISABLED) {} //Do Nothing if disabled
-		}
 
-		System.out.println(am);
-		if (autonomousCommand != null) autonomousCommand.start();
-		
-		camera.start();
+		if (autonomousCommand != null)
+			autonomousCommand.start();
 	}
 
 	public void autonomousPeriodic() {
 		SmartDashboard.putNumber("left position", Robot.drive.getLeftEncoder());
-		SmartDashboard.putNumber("right position ",Robot.drive.getRightEncoder()); 
+		SmartDashboard.putNumber("right position ", Robot.drive.getRightEncoder());
 		Scheduler.getInstance().run();
 	}
 
 	public void teleopInit() {
-		if (autonomousCommand != null) autonomousCommand.cancel();
+		if (autonomousCommand != null)
+			autonomousCommand.cancel();
+		camera.start();
 	}
 
 	public void teleopPeriodic() {
 		SmartDashboard.putNumber("left position", Robot.drive.getLeftEncoder());
-		SmartDashboard.putNumber("right position ",Robot.drive.getRightEncoder()); 
+		SmartDashboard.putNumber("right position ", Robot.drive.getRightEncoder());
 		Scheduler.getInstance().run();
 	}
 
 	public void testPeriodic() {
+		Scheduler.getInstance().run();
 	}
 }
--- src/org/usfirst/frc/team2340/robot/RobotUtils.java
+++ src/org/usfirst/frc/team2340/robot/RobotUtils.java
@@ -3,25 +3,29 @@ package org.usfirst.frc.team2340.robot;
 public class RobotUtils {
 	public enum AutoMode {
 		DISABLED,
-		DriveForward,
-		OneSwitch,
-		OneScale,
-		TwoSwitch,
-		ThreeSwitch,
-		ThreeScale
+		DRIVE_FORWARD,
+		ONE_SWITCH,
+		ONE_SCALE,
+		TWO_SWITCH,
+		THREE_SWITCH,
+		THREE_SCALE
 	}
-	
+
 	private static double wheelDiameter = 1;
 	private static double lengthOfRobot = 0;
+
 	public static void lengthOfRobot(double _lengthOfRobot) {
 		lengthOfRobot = _lengthOfRobot;
 	}
+
 	public static double getEncPositionFromIN(double distanceInInches) {
-		return (distanceInInches/(wheelDiameter * Math.PI))*1024*4;
+		return (distanceInInches / (wheelDiameter * Math.PI)) * 1024 * 4;
 	}
-	public static double distanceMinusRobot(double distance){
-		return distance-lengthOfRobot ;
+
+	public static double distanceMinusRobot(double distance) {
+		return distance - lengthOfRobot;
 	}
+
 	public static double getLengthOfRobot() {
 		return lengthOfRobot;
 	}
